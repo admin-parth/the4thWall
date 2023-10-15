349a70c9b419301c69ab453f5876801b28ea9be0
Parth Chauhan <parthchauhan05@gmail.com>
2023-10-15 02:05:23 -0400

    Empty Component update

--- components/element/emptydata/emptycomper.vue
+++ components/element/emptydata/emptycomper.vue
@@ -1,7 +1,7 @@
 <template>
-    <nuxt-link class="btn btn-gradient color-2" to="/listing/grid_view/2_grid_view/left-sidebar">Continue
-        Shopping</nuxt-link>
-    <img src="/image/empty.jpg" class="empty-cart w-100" alt="" />
+    <nuxt-link class="btn btn-gradient color-2" to="/listing/grid_view/2_grid_view/left-sidebar">
+        Continue Shopping
+    </nuxt-link>
 </template>
 
 <script setup lang="ts">
