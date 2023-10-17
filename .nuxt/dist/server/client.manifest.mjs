2cc4f566a70963ac0c91fd52b0f6d0b43a752554
Kunj2103 <kunjjansari1@gmail.com>
2023-10-17 20:58:11 +0530

    remove .nuxt from git tracking

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Parth/projects/Frontend/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Parth/projects/Frontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
