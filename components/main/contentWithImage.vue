bcb79d3dcb61491c89c3ac32feddb8cf99e5b993
Parth Chauhan <parthchauhan05@gmail.com>
2023-11-08 00:32:54 -0500

    Our specialities screen

--- components/main/contentWithImage.vue
+++ components/main/contentWithImage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="user-about">
         <div class="row">
-            <div class="col-xl-6 col-lg-6" :class="imageOnRight ? 'order-xl-2 order-lg-2' : 'order-xl-1 order-lg-1'">
+            <div class="col-xl-6 col-lg-6" :class="imageOnRight ? 'order-xl-2 order-lg-2' : 'order-xl-1 order-lg-1'" v-if="showImage">
                 <div class="about-image">
                     <div class="img-box side-left">
                         <img :src="`/image/${image1}`" class="img-fluid" alt="">
@@ -13,10 +13,11 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-6" :class="imageOnRight ? 'order-xl-1 order-lg-1' : 'order-xl-2 order-lg-2'">
+            <div :class="`${imageOnRight ? 'order-xl-1 order-lg-1' : 'order-xl-2 order-lg-2'} ${showImage ? 'col-xl-6 col-lg-6' : 'col-md-8 text-center offset-md-2'}`">
                 <div class="about-content">
                     <h3>{{ title }}</h3>
-                    <p class="font-roboto">{{ description }}</p>
+                    <p class="font-roboto mb-3">{{ description }}</p>
+                    <nuxt-link to="/main/our-specialities" class="btn btn-light-bg" v-if="showImage"><span>{{ 'view more '}}</span></nuxt-link>
                 </div>
             </div>
         </div>
@@ -29,6 +30,7 @@ defineProps({
     image2: String,
     title: String,
     description: String,
-    imageOnRight: {type: Boolean, default: false}
+    imageOnRight: {type: Boolean, default: false},
+    showImage: {type: Boolean, default: true}
 })
 </script>
--- pages/main/our-specialities.vue
+++ pages/main/our-specialities.vue
@@ -1,5 +1,57 @@
 <template>
-    <h1>{{ $t("whyChooseTheFourthWall.title") }}</h1>
+    <Head>
+        <Title>The 4th Wall</Title>
+    </Head>
+    <LayoutLayoutsLayout2>
+        <MainBannerBreadcrumb title="Our Specialities" first="Home" current="Our Specialities" />
+    
+        <section class="about-main">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="title-2 mb-5">
+                            <h2>{{ $t('whyChooseTheFourthWall.title') }}</h2>
+                            <svg class="title-svg">
+                                <use xlink:href="/svg/icons.svg#title-line"></use>
+                            </svg>
+                        </div>
+                        <MainContentWithImage 
+                            :title="$t('whyChooseTheFourthWall.subTitle1')"
+                            :description="$t('whyChooseTheFourthWall.para1')"
+                            :showImage="false"
+                         />
+                        <div class="hr p-3"><hr></div>
+                        <MainContentWithImage 
+                            :title="$t('whyChooseTheFourthWall.subTitle2')"
+                            :description="$t('whyChooseTheFourthWall.para2')"
+                            :showImage="false"
+                         />
+                        <div class="hr p-3"><hr></div>
+                        <MainContentWithImage 
+                            :title="$t('whyChooseTheFourthWall.subTitle3')"
+                            :description="$t('whyChooseTheFourthWall.para3')"
+                            :showImage="false"
+                         />
+                        <div class="hr p-3"><hr></div>
+                        <MainContentWithImage 
+                            :title="$t('whyChooseTheFourthWall.subTitle4')"
+                            :description="$t('whyChooseTheFourthWall.para4')"
+                            :showImage="false"
+                         />
+                        <div class="hr p-3"><hr></div>
+                        <MainContentWithImage 
+                            :title="$t('whyChooseTheFourthWall.subTitle5')"
+                            :description="$t('whyChooseTheFourthWall.para5')"
+                            :showImage="false"
+                         />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <ElementTaptopTapTop3 />
+    </LayoutLayoutsLayout2>
 </template>
 
 <script setup>
