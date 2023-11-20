caa164caeed3fd51176ceaa0308ae2f42917e97b
admin-parth <webdevservice.parth@gmail.com>
2023-11-20 23:10:03 +0530

    Add signup button in navbar

--- pages/main/signup.vue
+++ pages/main/signup.vue
@@ -1,11 +1,22 @@
-<template>
+<!-- <template>
     <ElementLoaderLoader1 />
     <LayoutCommonHeadersHeader3 />
     <ElementBradcrumbBridcrumb1 title="Sign Up" first="Home" current="Sign up" />
     <PageOtherpagesSignup />
     <LayoutCommonFootersFooter3 />
     <ElementTaptopTapTop1 />
+</template> -->
+<template>
+  <Head>
+      <Title>Sign Up | The 4th Wall</Title>
+  </Head>
+  <LayoutLayoutsLayout2>
+      <MainBannerBreadcrumb title="Sign Up" first="Home" current="Sign Up" />
+      <PageOtherpagesSignup />
+      <ElementTaptopTapTop3 />
+  </LayoutLayoutsLayout2>
 </template>
+
 
 <script setup lang="ts">
 useHead({
