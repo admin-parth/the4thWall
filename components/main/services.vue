02d02d61bd15427563261237dceca9b78510306a
Parth Chauhan <parthchauhan05@gmail.com>
2024-01-10 01:02:52 -0500

    Design process flip cards

--- components/main/services.vue
+++ components/main/services.vue
@@ -18,34 +18,39 @@
                 width: index === Services_data.length - 1 && 'fit-content',
               }"
             >
-              <div class="service-box">
-                <div class="hover-line">
-                  <svg class="service-icon">
-                    <use :xlink:href="item.svg"></use>
-                  </svg>
-                  <div>
-                    <svg class="icon-line-color">
-                      <use :xlink:href="item.svg1"></use>
-                    </svg>
+            <div class="flip-card-click">
+              <div class="flip-card-inner" tab-index="0">
+                <div class="flip-card-front">
+                  <div class="service-box">
+                    <div class="hover-line">
+                      <svg class="service-icon">
+                        <use :xlink:href="item.svg"></use>
+                      </svg>
+                      <div>
+                        <svg class="icon-line-color">
+                          <use :xlink:href="item.svg1"></use>
+                        </svg>
+                      </div>
+                    </div>
+                    <h3>{{ index + 1 }}. {{ item.title }}</h3>
+                    <button class="btn btn-light-bg">Learn more</button>
                   </div>
                 </div>
-                <!-- <nuxt-link to="/"> -->
-                <h3>{{ item.title }}</h3>
-                <!-- </nuxt-link> -->
-                <p class="font-roboto three-lines-only">{{ item.details }}</p>
-                <!-- <nuxt-link to="/main/design-process" class="btn btn-light-bg"
-                  ><span>{{ "view more " }}</span></nuxt-link
-                > -->
+                <div class="flip-card-back">
+                  <div class="service-box">
+                    <h3>{{ item.title }}</h3>
+                    <p class="font-roboto">{{ item.details }}</p>
+                  </div>
+                </div>
               </div>
+            </div>
               <div
                 v-if="index !== Services_data.length - 1"
                 class="arrow-1"
               ></div>
             </div>
             <div class="d-flex align-items-center justify-content-center mt-3">
-              <a class="btn btn-gradient btn-pill color-2 me-1"
-                >Book Your Session</a
-              >
+              <a class="btn btn-gradient btn-pill color-2 me-1">Book Your Session</a>
             </div>
           </div>
         </div>
@@ -56,6 +61,7 @@
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import { onMounted } from 'vue';
 const { t } = useI18n();
 const Services_data = [
   {
@@ -73,7 +79,7 @@ const Services_data = [
   {
     svg: "/svg/icons.svg#key",
     svg1: "/svg/icons.svg#line-straight",
-    title: "Worry-free installiation",
+    title: "Worry-free installation",
     details: t("ourDesignProcess.para3"),
   },
   {
@@ -83,8 +89,83 @@ const Services_data = [
     details: t("ourDesignProcess.para4"),
   },
 ];
+onMounted(() => {
+  document.querySelectorAll('.flip-card-click .flip-card-inner').forEach(function(item) {  
+    item.addEventListener('keypress', function(evt) { if (evt.keyCode == 13 || evt.keyCode == 32) { item.click(); } });
+  });
+  // click to flip
+  document.querySelectorAll('.flip-card-click').forEach(function(item) { 
+      item.addEventListener('click', function () { this.classList.toggle('flipped');  });
+  });
+})
+
 </script>
 <style scoped>
+.flip-card-click {
+  display: inline-block;
+  background-color: transparent;
+  width: 300px;
+  height: 350px;
+  margin-right: 12px;
+  margin-bottom: 12px;
+  perspective: 1000px;
+  transition: height 0.8s;
+}
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+.flip-card-click:focus .flip-card-inner,
+.flip-card-click:active .flip-card-inner,
+.flip-card-click.flipped .flip-card-inner {
+  transform: rotateY(180deg);
+  cursor: pointer;
+}
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+.flip-card-back {
+  flex-direction: column;
+  display: block;
+}
+@media (max-width: 575px) {
+  .flip-card-front, .flip-card-back {
+    display: block;
+  }
+
+  .flip-card-click:not(.flipped) {
+    height: 200px !important;
+  }
+}
+.flip-card-back .service-box{ 
+  padding: 30px 10px !important;
+  justify-content: flex-start !important;
+}
+.flip-card-back .service-box h3{
+  font-size: 16px;
+  text-align: center; 
+}
+.flip-card-front p, .flip-card-back p {
+  padding: 10px;
+  font-size: 12px;
+  color: black;
+  -webkit-line-clamp: initial !important;
+}
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+
 .arrow-1 {
   margin-top: 20px;
   width: 30px;
