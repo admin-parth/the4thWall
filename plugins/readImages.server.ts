1b45bcb7bdd3eb164b2f37c439c610065d4bd6a5
Kunj2103 <kunjjansari1@gmail.com>
2024-08-31 12:30:37 +0530

    testing

--- plugins/readImages.server.ts
+++ plugins/readImages.server.ts
@@ -1,43 +1,43 @@
-// ~/plugins/readImages.server.ts
 import { defineNuxtPlugin } from '#app';
 import { fileURLToPath } from 'url';
 import { dirname, resolve, join } from 'path';
 import fs from 'fs';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
+  if (process.server) {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
 
-  // Assuming the images are in the 'public/image' directory relative to the project root
-  const imagesDir = resolve(__dirname, '../public/image'); 
+    const imagesDir = resolve(__dirname, '../public/image'); 
 
-  const getImages = (dir) => {
-    const categories = [];
-    const categoryFolders = fs.readdirSync(dir);
+    const getImages = (dir) => {
+      const categories = [];
+      const categoryFolders = fs.readdirSync(dir);
 
-    categoryFolders.forEach((category) => {
-      const categoryPath = join(dir, category);
-      if (fs.statSync(categoryPath).isDirectory()) {
-        const images = fs.readdirSync(categoryPath);
-        categories.push({
-          name: category,
-          images: images.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file))
-        });
-      }
-    });
-    console.log(categories, 'from plugins function return');
-    return categories;
-  };
+      categoryFolders.forEach((category) => {
+        const categoryPath = join(dir, category);
+        if (fs.statSync(categoryPath).isDirectory()) {
+          const images = fs.readdirSync(categoryPath);
+          categories.push({
+            name: category,
+            images: images.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file))
+          });
+        }
+      });
+      return categories;
+    };
 
-  try {
-    const categories = getImages(imagesDir);
-    console.log(categories, 'from plugins');
-    nuxtApp.provide('imageCategories', categories);
-    nuxtApp.vueApp.provide('imageCategories', categories);
+    try {
+      const categories = getImages(imagesDir);
+      nuxtApp.provide('imageCategories', categories);
+      nuxtApp.vueApp.provide('imageCategories', categories);
 
-    const imageCategoriesState = useState('imageCategories', () => categories);
-    imageCategoriesState.value = categories;
-  } catch (error) {
-    console.error(`Error reading images directory: ${error.message}`);
+      const imageCategoriesState = useState('imageCategories', () => categories);
+      imageCategoriesState.value = categories;
+    } catch (error) {
+      console.error(`Error reading images directory: ${error.message}`);
+    }
+  }else{
+    console.log('only running on client side');
   }
 });
